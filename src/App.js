cd990caa2eaf4b93f0b62e8bdf95a6afcee0054a
Angel Flores <angelfloreschicago@gmail.com>
2021-06-06 18:46:51 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import "./App.css";
+import Header from "./components/Header.js";
+import EmployeeTable from "./components/EmployeeTable.js";
+import Container from 'react-bootstrap/Container';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState(false);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Container fluid>
+      <Header />
+      <SearchBar search={search} setSearch={setSearch} />
+      <EmployeeTable search={search} sort={sort} setSort={setSort}/>
+    </Container>
   );
 }
 
---
+++ src/components/EmployeeData.js
@@ -0,0 +1,17 @@
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+const Header = () => {
+    
+return (
+<Jumbotron fluid className="p-5 hero-text">
+  <h1>Employee Directory</h1>
+  <p>
+    Search hundreds of talented prospects waiting to connect today.
+  </p>
+</Jumbotron>
+
+)
+
+};
+
+export default Header;
