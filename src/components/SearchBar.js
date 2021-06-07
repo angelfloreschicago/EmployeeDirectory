3d48d61c34c5c0f900ee173de677b186b7b89304
Angel Flores <angelfloreschicago@gmail.com>
2021-06-06 22:52:47 -0500

    readme and minor updates

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar = ({ search, setSearch }) => {
   return (
     <Form>
       <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Search the database by country and sort asc. by clicking columns</Form.Label>
+        <Form.Label>Search the database by country:</Form.Label>
         <Form.Control
           name="search"
           type="text"
